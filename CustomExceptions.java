73c80dc10eec82713516f90130aa44c7810818a4
Muhammad Burhan Arshad <burhantheschoolboy@gmail.com>
2025-01-04 03:03:20 +0500

    Change

--- CustomExceptions.java
+++
@@ -1,8 +0,0 @@
-public class CustomExceptions {
-
-    public static class TaskNotFoundException extends Exception {
-        public TaskNotFoundException(String taskName) {
-            super("Task with name '" + taskName + "' not found.");
-        }
-    }
-}
